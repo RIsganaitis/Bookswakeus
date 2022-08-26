993a8db6d4efe55efd61cc4d5c48d697ffba58d7
RIsganaitis <ramunas.isganaitis@gmail.com>
2022-08-26 14:37:15 +0300

    add start files

---
+++ bookswakeus/src/main/java/codeAcademy/bookswakeus/BookswakeusApplication.java
@@ -0,0 +1,13 @@
+package codeAcademy.bookswakeus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookswakeusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookswakeusApplication.class, args);
+	}
+
+}
---
+++ bookswakeus/src/test/java/codeAcademy/bookswakeus/BookswakeusApplicationTests.java
@@ -0,0 +1,13 @@
+package codeAcademy.bookswakeus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookswakeusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
