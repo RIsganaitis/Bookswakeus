3a66fd945062194986b7b6ae00b61b9823dfcc63
RIsganaitis <ramunas.isganaitis@gmail.com>
2022-09-19 15:55:16 +0300

    add postrge db

--- bookswakeus/src/main/java/codeAcademy/bookswakeus/books/BooksService.java
+++ bookswakeus/src/main/java/codeAcademy/bookswakeus/books/BooksService.java
@@ -4,7 +4,6 @@ import codeAcademy.bookswakeus.books.errors.BookNotFoundException;
 import codeAcademy.bookswakeus.books.repos.JpaBooksRepos;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
